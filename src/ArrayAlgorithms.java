52e18305a3a5750d7c17df55b39c402b1aa53e35
BT_LIBRARY_10 <BT_LIBRARY_10@bktech.local>
2023-01-13 08:45:53 -0500

    working...

--- src/ArrayAlgorithms.java
+++ src/ArrayAlgorithms.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class ArrayAlgorithms {
+public class ArrayAlgorithms {
+    /** Returnes the longest String(s) in an array of Strings.
+     *      *  If multiple strings are tied for longest length, return the last
+     *      *  element in th list with that length.
+     *
+     *  This method does NOT modify the original array.
+     *  PRECONDITION: stringList.length > 0
+     */
+    public static String longestString(String[] stringList) {
+        String longStr = "";
+        for (String str : stringList) {
+            if (str.length() >= longStr.length()) {
+                longStr = str;
+            }
+        }
+        return longStr;
+    }
+
+    /** Returns true if ANY of the ints in numList are positive, otherwise,
+     *  returns false; use an early return to improve efficiency!
+     *  note: 0 is not considered positive
+     *
+     *  This method does NOT modify the original array.
+     *  PRECONDITION: numList.length > 0
+     */
+    public static boolean containsPositive(int[] numList)
+    {
+        for (int num : numList) {
+            if (num > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /** Returns a NEW array of Strings that contains all words from the original
+     *  array of strings in lowercase (use the toLowerCase() string method)
+     *
+     *  This method does NOT modify the original array
+     *  PRECONDITION: wordList.length > 0
+     */
+    public static String[] makeLowercase(String[] wordList) {
+        String[] arr = new String[wordList.length];
+        for (int i = 0; i < wordList.length; i++) {
+            arr += wordList[i].toLowerCase();
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] nums3 = {-6, -4, 0, -3, -1, -2, 5};
+        boolean positive = ArrayAlgorithms.containsPositive(nums3);
+        System.out.println(positive);
+        int[] nums4 = {-6, -4, 0, -3, -1, -2, -5};
+        boolean positive2 = ArrayAlgorithms.containsPositive(nums4);
+        System.out.println(positive2);
+        int[] nums5 = {6, -4, -8, -3, -1, -2, -5};
+        boolean positive3 = ArrayAlgorithms.containsPositive(nums5);
+        System.out.println(positive3);
+        int[] nums6 = {6, 4, 8, 3, 1, 2, 5};
+        boolean positive4 = ArrayAlgorithms.containsPositive(nums6);
+        System.out.println(positive4);
+
+
+
+
+
     }
 }
