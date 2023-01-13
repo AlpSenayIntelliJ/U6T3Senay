8bf6f6e8d2232cd536a05b34f16f567af4d7f9a1
BT_3W2_18 <alps2789@nycstudents.net>
2023-01-13 12:16:58 -0500

    done!

--- src/ArrayAlgorithms.java
+++ src/ArrayAlgorithms.java
@@ -42,8 +42,110 @@ public class ArrayAlgorithms {
     public static String[] makeLowercase(String[] wordList) {
         String[] arr = new String[wordList.length];
         for (int i = 0; i < wordList.length; i++) {
-            arr += wordList[i].toLowerCase();
+            arr[i] = wordList[i].toLowerCase();
         }
+        return arr;
+    }
+
+    /** Returns the number of Strings in stringList that have an exclamation point
+     *  ("!") at the end.
+     *
+     *  This method does NOT modify the original array.
+     *  PRECONDITION: stringList.length > 0
+     */
+    public static int endsInExclamations(String[] stringList) {
+        int count = 0;
+        for (String str : stringList) {
+            if (str.charAt(str.length() - 1) == '!') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /** Updates each String in the array to be all uppercase; this method does
+     *  not return a value
+     *
+     *  This method MODIFIED the original array
+     *  PRECONDITION: wordList.length > 0
+     */
+    public static void makeUppercase(String[] wordList) {
+    /*for (int i = 0; i < wordList.length; i++) {
+        wordList[i] = wordList[i].toUpperCase();
+    }*/
+        String[] upperStrs = new String[wordList.length];
+
+        for (int i = 0; i < wordList.length; i++) {
+            upperStrs[i] = wordList[i].toUpperCase();
+        }
+
+        // set wordList to the newList
+        wordList = upperStrs;
+
+    }
+
+    /** Returns a NEW array of ints that contains the higher value of each pair
+     *  of elements at the same index in two other equally-sized arrays of ints
+     *  For example, if intArr1 = {2, 5, 6, 1, 0} and intArr2 = {1, 7, 6, -2, 8},
+     *  this method returns the array {2, 7, 6, 1, 8}, where each value is the
+     *  higher of the two at that respective index in the two original arrays
+     *
+     *  This method does NOT modify either of the original arrays
+     *  PRECONDITION: intArr1.length == intArr2.length
+     */
+    public static int[] arrayMaximums(int[] intArr1, int[] intArr2) {
+        int[] arr = new int[intArr1.length];
+        for (int i = 0; i < intArr1.length; i++) {
+            if (intArr1[i] >= intArr2[i]) {
+                arr[i] = intArr1[i];
+            }
+            if (intArr1[i] < intArr2[i]) {
+                arr[i] = intArr2[i];
+            }
+        }
+        return arr;
+    }
+
+    /** Returns the number of elements in the numList array that have the same
+     *  value as the element immediately adjacent to the right in the array.
+     *
+     *  For example, given the array {4, 7, 7, 3, 2, 2, 4, 4, 4, 6, 4, 6, 6}
+     *  this method would return 5, since there are five numbers in the array that
+     *  are equal to the number immediately to the right (shown below in blue):
+     *                    {4, 7, 7, 3, 2, 2, 4, 4, 4, 6, 4, 6, 6}
+     *
+     *  This method does NOT modify the original array.
+     *  PRECONDITION: numList.length > 0
+     */
+    public static int countConsecutiveDoubles(int[] numList) {
+        int count = 0;
+        for (int i = 0; i < numList.length-1; i++) {
+            if (numList[i] == numList[i+1]) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /** Returns the "longest streak" of the array; a streak is the number of times
+     *  a particular number is repeated consecutively, one after the other
+     *
+     *  This method does NOT modify the original array.
+     *  PRECONDITION: numList.length > 0
+     */
+    public static int longestStreak(int[] nums) {
+        int maxStreak = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int streakLength = 1;
+            while (i < nums.length - 1 && nums[i] == nums[i+1]) {
+                streakLength++;
+                i++;
+            }
+            if (streakLength > maxStreak) {
+                maxStreak = streakLength;
+            }
+        }
+        return maxStreak;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums3 = {-6, -4, 0, -3, -1, -2, 5};
-        boolean positive = ArrayAlgorithms.containsPositive(nums3);
-        System.out.println(positive);
-        int[] nums4 = {-6, -4, 0, -3, -1, -2, -5};
-        boolean positive2 = ArrayAlgorithms.containsPositive(nums4);
-        System.out.println(positive2);
-        int[] nums5 = {6, -4, -8, -3, -1, -2, -5};
-        boolean positive3 = ArrayAlgorithms.containsPositive(nums5);
-        System.out.println(positive3);
-        int[] nums6 = {6, 4, 8, 3, 1, 2, 5};
-        boolean positive4 = ArrayAlgorithms.containsPositive(nums6);
-        System.out.println(positive4);
+        String[] strings9 = {"HELlo", "Halo", "WHAT!", "Adam", "what", "booyAH", "WHY?", "for"};
+        ArrayAlgorithms.makeUppercase(strings9);
+        for (String str : strings9) {
+            System.out.print(str + " ");
+        }
+        System.out.println();
+
+
+
+
+
 
 
 
